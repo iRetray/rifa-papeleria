f0cfbe75dac8c8ddc0d43675ce92a577fe1e0f97
Julian Camilo Cruz Sanchez <julian20141016@gmail.com>
2025-10-01 14:42:21 -0500

    Updated Netlify project

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Optimizaciones para Firebase
-    __FIREBASE_DEFAULTS__: JSON.stringify({
-      projectId: 'rifa-papeleria'
-    }),
-  },
+
   build: {
     // Optimizaciones críticas para reducir bundle size
     target: 'es2015',
